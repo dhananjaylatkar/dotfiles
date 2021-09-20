cf4ca70ceee8f651a7163fb700e6eab2f274ecfd
Dhananjay <dhananjay.latkar@gmail.com>
2021-09-20 21:52:46 +0530

    nvim: nvim-cmp lsp completion capabilities.

--- nvim/.config/nvim/lua/plugin/lspconfig.lua
+++ nvim/.config/nvim/lua/plugin/lspconfig.lua
@@ -5,8 +5,12 @@ local on_attach = function(_client, bufnr)
   vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 end
 
+-- nvim-cmp supports additional completion capabilities
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+
 -- Enable the following language servers
 local servers = { "clangd", "rust_analyzer", "pyright", "tsserver" }
 for _, lsp in ipairs(servers) do
-  nvim_lsp[lsp].setup({ on_attach = on_attach })
+  nvim_lsp[lsp].setup({ on_attach = on_attach, capabilities = capabilities })
 end
