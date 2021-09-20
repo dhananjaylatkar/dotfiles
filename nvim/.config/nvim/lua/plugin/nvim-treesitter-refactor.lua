30d80856564847197400ed5338a65a6ee2269d26
Dhananjay <dhananjay.latkar@gmail.com>
2021-09-20 21:46:33 +0530

    nvim: disable scope highlight

--- nvim/.config/nvim/lua/plugin/nvim-treesitter-refactor.lua
+++ nvim/.config/nvim/lua/plugin/nvim-treesitter-refactor.lua
@@ -3,7 +3,7 @@
 require("nvim-treesitter.configs").setup({
   refactor = {
     highlight_definitions = { enable = true },
-    highlight_current_scope = { enable = true },
+    -- highlight_current_scope = { enable = true },
     smart_rename = {
       enable = true,
       keymaps = {
