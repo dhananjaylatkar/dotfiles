1425aebfb616a41e3e1608432706534838b16232
Dhananjay <dhananjay.latkar@gmail.com>
2022-02-12 22:23:19 +0530

    nvim: nvim-cmp Tab key fix

--- nvim/.config/nvim/lua/plugin/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugin/nvim-cmp.lua
@@ -2,11 +2,18 @@
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = "menuone,noinsert"
-
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0
+    and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]
+        :sub(col, col)
+        :match("%s")
+      == nil
+end
 -- luasnip setup
 local luasnip = require("luasnip")
-
 local cmp = require("cmp")
+
 cmp.setup({
   snippet = {
     expand = function(args)
@@ -24,40 +31,27 @@ cmp.setup({
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     }),
-    ["<Tab>"] = function(fallback)
+    ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
       elseif luasnip.expand_or_jumpable() then
-        vim.fn.feedkeys(
-          vim.api.nvim_replace_termcodes(
-            "<Plug>luasnip-expand-or-jump",
-            true,
-            true,
-            true
-          ),
-          ""
-        )
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
       else
         fallback()
       end
-    end,
-    ["<S-Tab>"] = function(fallback)
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
       elseif luasnip.jumpable(-1) then
-        vim.fn.feedkeys(
-          vim.api.nvim_replace_termcodes(
-            "<Plug>luasnip-jump-prev",
-            true,
-            true,
-            true
-          ),
-          ""
-        )
+        luasnip.jump(-1)
       else
         fallback()
       end
-    end,
+    end, { "i", "s" }),
   },
   sources = {
     { name = "nvim_lsp" },
