05ddd236082d85ddf7c8078bdabe89118fb2fcc2
Dhananjay <dhananjay.latkar@gmail.com>
2022-02-23 21:19:20 +0530

    nvim: try to fix cmp jump

--- nvim/.config/nvim/lua/plugin/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugin/nvim-cmp.lua
@@ -26,6 +26,20 @@ cmp.setup({
     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-.>"] = cmp.mapping(function(fallback)
+      if luasnip.jumpable() then
+        luasnip.jump()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ["<C-,>"] = cmp.mapping(function(fallback)
+      if luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
     ["<C-e>"] = cmp.mapping.close(),
     ["<CR>"] = cmp.mapping.confirm({
       behavior = cmp.ConfirmBehavior.Replace,
@@ -34,10 +48,8 @@ cmp.setup({
     ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      elseif has_words_before() then
-        cmp.complete()
+      elseif luasnip.expandable() then
+        luasnip.expand()
       else
         fallback()
       end
@@ -46,10 +58,6 @@ cmp.setup({
     ["<S-Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
       end
     end, { "i", "s" }),
   },
