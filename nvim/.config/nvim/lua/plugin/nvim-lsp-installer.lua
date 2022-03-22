4f2afdb97570e6ef667eb105b6ad910daa8a17e2
Dhananjay <dhananjay.latkar@gmail.com>
2022-03-22 23:02:45 +0530

    nvim: get rid of vim warnings in lua config

--- nvim/.config/nvim/lua/plugin/nvim-lsp-installer.lua
+++ nvim/.config/nvim/lua/plugin/nvim-lsp-installer.lua
@@ -23,9 +23,9 @@ lsp_installer.on_server_ready(function(server)
 	local opts = {}
 
 	-- (optional) Customize the options passed to the server
-	-- if server.name == "tsserver" then
-	--     opts.root_dir = function() ... end
-	-- end
+	if server.name == "sumneko_lua" then
+		opts.settings = { Lua = { diagnostics = { globals = { "vim" } } } }
+	end
 
 	-- This setup() function is exactly the same as lspconfig's setup function (:help lspconfig-quickstart)
 	server:setup(opts)
