b52c50ee3e4fbed0cea0129fb0d41d97db1c3da7
Dhananjay <dhananjay.latkar@gmail.com>
2022-05-05 15:39:19 +0530

    nvim: nvim-tree new options

--- nvim/.config/nvim/lua/plugin/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugin/nvim-tree.lua
@@ -1,18 +1,61 @@
 require("nvim-tree").setup({
+	auto_reload_on_write = true,
 	disable_netrw = true,
+	hijack_cursor = false,
 	hijack_netrw = true,
+	hijack_unnamed_buffer_when_opening = false,
+	ignore_buffer_on_setup = false,
 	open_on_setup = false,
-	ignore_ft_on_setup = {},
-	update_to_buf_dir = {
+	open_on_setup_file = false,
+	open_on_tab = false,
+	sort_by = "name",
+	update_cwd = false,
+	view = {
+		width = 30,
+		height = 30,
+		hide_root_folder = false,
+		side = "left",
+		preserve_window_proportions = false,
+		number = false,
+		relativenumber = false,
+		signcolumn = "yes",
+		mappings = {
+			custom_only = false,
+			list = {
+				-- user mappings go here
+			},
+		},
+	},
+	renderer = {
+		indent_markers = {
+			enable = false,
+			icons = {
+				corner = "└ ",
+				edge = "│ ",
+				none = "  ",
+			},
+		},
+		icons = {
+			webdev_colors = true,
+		},
+	},
+	hijack_directories = {
 		enable = true,
 		auto_open = true,
 	},
-	auto_close = false,
-	open_on_tab = false,
-	hijack_cursor = false,
-	update_cwd = false,
+	update_focused_file = {
+		enable = false,
+		update_cwd = false,
+		ignore_list = {},
+	},
+	ignore_ft_on_setup = {},
+	system_open = {
+		cmd = "",
+		args = {},
+	},
 	diagnostics = {
 		enable = false,
+		show_on_dirs = false,
 		icons = {
 			hint = "",
 			info = "",
@@ -20,23 +63,50 @@ require("nvim-tree").setup({
 			error = "",
 		},
 	},
-	update_focused_file = {
+	filters = {
+		dotfiles = false,
+		custom = {},
+		exclude = {},
+	},
+	git = {
+		enable = true,
+		ignore = true,
+		timeout = 400,
+	},
+	actions = {
+		use_system_clipboard = true,
+		change_dir = {
+			enable = true,
+			global = false,
+			restrict_above_cwd = false,
+		},
+		open_file = {
+			quit_on_open = false,
+			resize_window = false,
+			window_picker = {
+				enable = true,
+				chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+				exclude = {
+					filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
+					buftype = { "nofile", "terminal", "help" },
+				},
+			},
+		},
+	},
+	trash = {
+		cmd = "trash",
+		require_confirm = true,
+	},
+	log = {
 		enable = false,
-		update_cwd = false,
-		ignore_list = {},
-	},
-	system_open = {
-		cmd = nil,
-		args = {},
-	},
-	view = {
-		width = 30,
-		height = 30,
-		side = "left",
-		auto_resize = false,
-		mappings = {
-			custom_only = false,
-			list = {},
+		truncate = false,
+		types = {
+			all = false,
+			config = false,
+			copy_paste = false,
+			diagnostics = false,
+			git = false,
+			profile = false,
 		},
 	},
 })
