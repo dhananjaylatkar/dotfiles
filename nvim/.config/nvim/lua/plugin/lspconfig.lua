bbc941f4b1b52a6e55898ab43917a6044b3a6ffb
Dhananjay <dhananjay.latkar@gmail.com>
2022-10-21 13:19:32 +0530

    nvim: update deprecated function

--- nvim/.config/nvim/lua/plugin/lspconfig.lua
+++ nvim/.config/nvim/lua/plugin/lspconfig.lua
@@ -7,7 +7,7 @@ end
 
 -- nvim-cmp supports additional completion capabilities
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 ----------------------------------------------
 -- Using nvim-lsp-installer for LSP servers --
