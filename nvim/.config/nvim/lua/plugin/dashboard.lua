ffd1e8c8736555322f5b158a26d8ba7905bf81c5
Dhananjay <dhananjay.latkar@gmail.com>
2023-03-11 01:01:19 +0530

    nvim: dashboard - doom theme

--- nvim/.config/nvim/lua/plugin/dashboard.lua
+++ nvim/.config/nvim/lua/plugin/dashboard.lua
@@ -1,40 +1,46 @@
 -- Dashboard config
 
 require("dashboard").setup({
-	theme = "hyper",
+	theme = "doom",
 	config = {
 		header = CONFIG.dashboard.custom_header,
 		footer = CONFIG.dashboard.custom_footer,
-		project = { limit = 5, action = 'Telescope find_files hidden=true cwd='},
-		mru = { limit = 5},
-		-- week_header = {
-		-- 	enable = true,
-		-- },
-		shortcut = {
-			{
-				desc = " Recents",
-				group = "String",
-				action = "Telescope oldfiles",
-				key = "1",
-			},
-			{
-				desc = " Files",
-				group = "Label",
-				action = "Telescope find_files hidden=true",
-				key = "2",
-			},
-			{
-				desc = " Project",
-				group = "DiagnosticHint",
-				action = "Telescope project",
-				key = "3",
-			},
-			{
-				desc = " Grep",
-				group = "Number",
-				action = "Telescope live_grep",
-				key = "4",
-			},
-		},
+        center = {
+          {
+            icon = 'ﭯ ',
+            desc = 'Recent Files           ',
+            key = 'r',
+            keymap = 'SPC f r',
+            action = 'Telescope oldfiles'
+          },
+          {
+            icon = ' ',
+            desc = 'Open Project           ',
+            key = 'p',
+            keymap = 'SPC p p',
+            action = 'Telescope project'
+          },
+          {
+            icon = ' ',
+            desc = 'Find Files           ',
+            key = 'b',
+            keymap = 'SPC f f',
+            action = 'Telescope find_files hidden=true'
+          },
+          {
+            icon = ' ',
+            desc = 'Find Text',
+            key = 't',
+            keymap = 'SPC s p',
+            action = 'Telescope live_grep'
+          },
+          {
+            icon = ' ',
+            desc = 'Config file',
+            key = '.',
+            keymap = 'SPC k c',
+            action = 'e ~/.config/nvim/lua/' .. CONFIG_FILE
+          },
+        },
 	},
 })
