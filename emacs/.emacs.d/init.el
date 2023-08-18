bf334be9c47278c832542ed983c017fa6fe3102d
Dhananjay <dhananjay.latkar@gmail.com>
2023-08-18 17:47:38 +0530

    emacs: tailored for org-mode

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -16,82 +16,60 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+;; UI options
 (setq inhibit-startup-message t)
-(scroll-bar-mode -1)                                    ; Disable visible scrollbar
-(tool-bar-mode -1)                                      ; Disable the toolbar
-(tooltip-mode -1)                                       ; Disable tooltips
-(set-fringe-mode 10)                                    ; Give some breathing room
-(menu-bar-mode -1)                                      ; Disable the menu bar
-(setq visible-bell t)                                   ; Set up the visible bell
-(add-hook 'text-mode-hook 'visual-line-mode)            ; wrap lines
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(tooltip-mode -1)
+(set-fringe-mode 10)
+(menu-bar-mode -1)
+(setq visible-bell t)
+(add-hook 'text-mode-hook 'visual-line-mode)
+
+;; Line numbers
 (column-number-mode)
 (global-display-line-numbers-mode t)
-
 ;; Disable line numbers for some modes
 (dolist (mode '(org-mode-hook
                 term-mode-hook
                 shell-mode-hook
-	              treemacs-mode-hook
+	        treemacs-mode-hook
                 eshell-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
-(defvar dha/default-font-size 150)
-(defvar dha/default-variable-font-size 150)
-
-(set-face-attribute 'default nil :font "Fira Code" :height dha/default-font-size)
-
-;; Set the fixed pitch face
-(set-face-attribute 'fixed-pitch nil :font "Fira Code" :height dha/default-font-size)
-
-;; All the icons for Doom modeline
-(use-package all-the-icons)
-
-(use-package doom-themes
-  :init (load-theme 'doom-dracula t))
-
-(use-package all-the-icons)
-
-(use-package doom-modeline
-  :init 
-    (doom-modeline-mode 1)
-  :custom ( 
-    (doom-modeline-height 5)
-    (doom-modeline-buffer-file-name-style 'auto)
-    (doom-modeline-project-detection 'project)))
-
-(use-package which-key
-  :init (which-key-mode)
-  :diminish which-key-mode
-  :config
-  (setq which-key-idle-delay 1))
-
-(use-package helpful
-  :custom
-  (counsel-describe-function-function #'helpful-callable)
-  (counsel-describe-variable-function #'helpful-variable)
-  :bind
-  ([remap describe-function] . counsel-describe-function)
-  ([remap describe-command] . helpful-command)
-  ([remap describe-variable] . counsel-describe-variable)
-  ([remap describe-key] . helpful-key))
-
+;; Disable tmp files
 (setq create-lockfiles nil)
 (setq auto-save-default nil)
 (setq make-backup-files nil)
 
-(global-set-key (kbd "<escape>") 'keyboard-escape-quit) ; Make ESC quit prompts
+;;;;;;;;;;;;;;;;;;;;;;;; Packages ;;;;;;;;;;;;;;;;;;;;;;;; 
+
+;; diminish (keep mode-line clean)
+(use-package diminish)
+
+;; which-key
+(use-package which-key
+  :diminish
+  :init
+  (setq which-key-idle-delay 0.1)
+  (which-key-mode))
 
 (use-package general
   :config
-  (general-create-definer dha/leader-keys
+  (general-create-definer djay/leader-keys
     :keymaps '(normal insert visual emacs)
     :prefix "SPC"
     :global-prefix "C-SPC"))
 
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit) ; Make ESC quit prompts
+
+;; undo-tree
 (use-package undo-tree
+  :diminish
   :config
   (global-undo-tree-mode))
 
+;; evil
 (use-package evil
   :init
   (setq evil-want-integration t)
@@ -103,7 +81,6 @@
   (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
   (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
 
-  ;; Use visual line motions even outside of visual-line-mode buffers
   (evil-global-set-key 'motion "j" 'evil-next-visual-line)
   (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
   (evil-set-undo-system 'undo-tree)
@@ -111,164 +88,58 @@
   (evil-set-initial-state 'messages-buffer-mode 'normal)
   (evil-set-initial-state 'dashboard-mode 'normal))
 
-(use-package evil-collection
-  :after evil
+;; org-mode
+(defun djay/org-mode-setup ()
+  (org-indent-mode)
+  (visual-line-mode 1))
+
+(use-package org
+  :hook (org-mode . djay/org-mode-setup)
   :config
-  (evil-collection-init))
-
-(use-package ivy
-  :diminish
-  :bind (("C-s" . swiper)
-         :map ivy-minibuffer-map
-         ("TAB" . ivy-alt-done)
-         ("C-l" . ivy-alt-done)
-         ("C-j" . ivy-next-line)
-         ("C-k" . ivy-previous-line)
-         :map ivy-switch-buffer-map
-         ("C-k" . ivy-previous-line)
-         ("C-l" . ivy-done)
-         ("C-d" . ivy-switch-buffer-kill)
-         :map ivy-reverse-i-search-map
-         ("C-k" . ivy-previous-line)
-         ("C-d" . ivy-reverse-i-search-kill))
-  :config
-  (ivy-mode 1))
-
-(use-package ivy-rich
-  :init
-  (ivy-rich-mode 1))
-
-(use-package counsel
-  :bind 
-  (("M-x" . counsel-M-x)
-  ("C-x b" . counsel-ibuffer)
-  ("C-x C-f" . counsel-find-file))
-  :config
-  (counsel-mode 1))
-
-(dha/leader-keys
-  "fb" '(counsel-ibuffer :which-key "find buffers")
-  "ff" '(counsel-find-file :which-key "find files"))
-
-(use-package hydra)
-
-(defhydra hydra-text-scale (:timeout 4)
-  "scale text"
-  ("=" text-scale-increase "in")
-  ("-" text-scale-decrease "out")
-  ("0" nil "finished" :exit t))
-
-(dha/leader-keys
-  "ts" '(hydra-text-scale/body :which-key "scale text"))
-
-;; Set faces for heading levels
-(dolist 
-  (face 
-    '((org-level-1 . 1.2)
-     (org-level-2 . 1.1)
-     (org-level-3 . 1.05)
-     (org-level-4 . 1.0)
-     (org-level-5 . 1.1)
-     (org-level-6 . 1.1)
-     (org-level-7 . 1.1)
-     (org-level-8 . 1.1))))
-
-(defun dha/org-mode-setup ()
-    (org-indent-mode)
-    (visual-line-mode 1))
-
-  (use-package org
-    :hook (org-mode . dha/org-mode-setup)
-    :config
-    (setq org-ellipsis " ▾"))
+  (setq org-ellipsis " ▾")
+  (setq org-log-done t)
+  (setq org-agenda-files (list "~/code/org/tasks.org")))
 
 (use-package org-bullets
   :after org
   :hook (org-mode . org-bullets-mode))
 
-(org-babel-do-load-languages
-  'org-babel-load-languages
-  '((emacs-lisp . t)
-    (python . t)))
+(djay/leader-keys
+  "o"  '(:which-key "org")
+  "ol" '(org-store-link :which-key "store link")
+  "oa" '(org-agenda :which-key "agenda")
+  "oo" '((lambda () (interactive) (find-file "~/code/org/tasks.org")) :which-key "open"))
 
-(push '("conf-unix" . conf-unix) org-src-lang-modes)
-
-(require 'org-tempo)
-
-(add-to-list 'org-structure-template-alist '("sh" . "src shell"))
-(add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
-(add-to-list 'org-structure-template-alist '("py" . "src python"))
-
-;; Automatically tangle our Emacs.org config file when we save it
-(defun dha/org-babel-tangle-config ()
-  (when (string-equal (buffer-file-name)
-                      (expand-file-name "~/.emacs.d/emacs.org"))
-    ;; Dynamic scoping to the rescue
-    (let ((org-confirm-babel-evaluate nil))
-      (org-babel-tangle))))
-
-(add-hook 'org-mode-hook (lambda () (add-hook 'after-save-hook #'dha/org-babel-tangle-config)))
-
-(defun efs/lsp-mode-setup ()
-  (setq lsp-headerline-breadcrumb-segments '(path-up-to-project file symbols))
-  (lsp-headerline-breadcrumb-mode))
-
-(use-package lsp-mode
-  :commands (lsp lsp-deferred)
-  :hook (lsp-mode . efs/lsp-mode-setup)
-  :init
-  (setq lsp-keymap-prefix "C-c l")  ;; Or 'C-l', 's-l'
+;; counsel
+(use-package counsel
+  :diminish
+  :bind 
+  (("M-x" . counsel-M-x)
+   ("C-x b" . counsel-ibuffer)
+   ("C-x C-f" . counsel-find-file))
   :config
-  (lsp-enable-which-key-integration t))
+  (counsel-mode 1))
 
-(use-package lsp-ui
-  :hook (lsp-mode . lsp-ui-mode)
-  :custom
-  (lsp-ui-doc-position 'bottom))
+(djay/leader-keys
+  "f"  '(:which-key "find")
+  "fb" '(counsel-ibuffer :which-key "buffers")
+  "ff" '(counsel-find-file :which-key "files"))
 
-(use-package lsp-ivy)
-
-(use-package typescript-mode
-  :mode "\\.js\\'"
-  :hook (typescript-mode . lsp-deferred)
+;; ivy (completion framework)
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+	 :map ivy-minibuffer-map
+	 ("TAB" . ivy-alt-done)
+	 ("C-l" . ivy-alt-done)
+	 ("C-j" . ivy-next-line)
+	 ("C-k" . ivy-previous-line)
+	 :map ivy-switch-buffer-map
+	 ("C-k" . ivy-previous-line)
+	 ("C-l" . ivy-done)
+	 ("C-d" . ivy-switch-buffer-kill)
+	 :map ivy-reverse-i-search-map
+	 ("C-k" . ivy-previous-line)
+	 ("C-d" . ivy-reverse-i-search-kill))
   :config
-  (setq typescript-indent-level 2))
-
-(use-package company
-  :after lsp-mode
-  :hook (lsp-mode . company-mode)
-  :bind (:map company-active-map
-  ("<tab>" . company-complete-selection))
-  (:map lsp-mode-map
-  ("<tab>" . company-indent-or-complete-common))
-  :custom
-  (company-minimum-prefix-length 1)
-  (company-idle-delay 0.0))
-
-(use-package company-box
-  :hook (company-mode . company-box-mode))
-
-(use-package projectile
-  :diminish projectile-mode
-  :config (projectile-mode)
-  :custom ((projectile-completion-system 'ivy))
-  :init
-  (when (file-directory-p "~/code")
-    (setq projectile-project-search-path '("~/code")))
-  (setq projectile-switch-project-action #'projectile-dired)
-  (dha/leader-keys
-    "p" '(projectile-command-map :which-key "Projectile")
-    ))
-
-(use-package counsel-projectile
-  :config (counsel-projectile-mode))
-
-(setq projectile-enable-caching t)
-
-(use-package magit)
-
-(use-package rainbow-delimiters
-  :hook (prog-mode . rainbow-delimiters-mode))
-
-(use-package evil-nerd-commenter
-  :bind ("M-/" . evilnc-comment-or-uncomment-lines))
+  (ivy-mode 1))
