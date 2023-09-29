22b3737ce293061566099c1bf06da306875b9997
Dhananjay <dhananjay.latkar@gmail.com>
2023-09-29 16:06:48 +0530

    nvim: gruvbox-material performance mode

--- nvim/.config/nvim/lua/colors/gruvbox-material.lua
+++ nvim/.config/nvim/lua/colors/gruvbox-material.lua
@@ -2,5 +2,5 @@
 vim.g.gruvbox_material_enable_italic = 1
 vim.g.gruvbox_material_background = "hard" -- hard, soft, medium
 vim.g.gruvbox_material_palette = "mix" -- original, mix, material
-vim.g.gruvbox_material_enable_italic = 1
+vim.g.gruvbox_material_better_performace = 1
 vim.cmd([[colorscheme gruvbox-material]])
