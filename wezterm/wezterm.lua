485320e2069546d841e9ff5ea803e755fbf1b1eb
Dhananjay <dhananjay.latkar@gmail.com>
2024-07-15 11:54:45 +0530

    wezterm: maximize on startup and use PS on win

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,5 +1,6 @@
 local wezterm = require("wezterm")
 local act = wezterm.action
+local mux = wezterm.mux
 
 local config = wezterm.config_builder()
 
@@ -9,10 +10,21 @@ config.font = wezterm.font("JetBrains Mono")
 config.font_size = 14.0
 
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
-config.window_decorations = "NONE"
+config.window_decorations = "RESIZE"
 
 -- config.enable_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
+
+-- Maximize on startup
+wezterm.on('gui-attached', function(_)
+  -- maximize all displayed windows on startup
+  local workspace = mux.get_active_workspace()
+  for _, window in ipairs(mux.all_windows()) do
+    if window:get_workspace() == workspace then
+      window:gui_window():maximize()
+    end
+  end
+end)
 
 config.mouse_bindings = {
 	-- Right click to paste
@@ -22,5 +34,10 @@ config.mouse_bindings = {
 		action = act.PasteFrom("Clipboard"),
 	},
 }
+
+
+if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
+	config.default_prog = { 'powershell.exe' }
+end
 
 return config
