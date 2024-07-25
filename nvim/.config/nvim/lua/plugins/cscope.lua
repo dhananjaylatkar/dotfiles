1219c17bf858f949ea61e07b35fce74fc68a5043
Dhananjay <dhananjay.latkar@gmail.com>
2024-07-25 16:59:40 +0530

    nvim: cscope use min.pick

--- nvim/.config/nvim/lua/plugins/cscope.lua
+++ nvim/.config/nvim/lua/plugins/cscope.lua
@@ -2,12 +2,13 @@
 return {
   -- dir = "~/code/cscope_maps.nvim",
   "dhananjaylatkar/cscope_maps.nvim",
-  ft = { "c", "h", "cpp", "starter" },
-  cmd = { "Cscope", "Cstag" },
+  ft = { "c", "h", "cpp", "ministarter" },
+  cmd = { "Cscope", "Cstag", "Cs" },
   keys = "<C-]>",
   opts = {
     cscope = {
-      picker = "telescope",
+      picker = "mini-pick",
+      project_rooter = { enable = true },
     },
   },
   enabled = vim.g.dha.conf.enable.cscope_maps,
