da0fba3a757f0e3c829c6e64b00a8da8018d3a1f
Dhananjay <dhananjay.latkar@gmail.com>
2024-09-23 21:23:57 +0530

    nvim: only use italics for comments

--- nvim/.config/nvim/lua/colors/catppuccin.lua
+++ nvim/.config/nvim/lua/colors/catppuccin.lua
@@ -18,9 +18,9 @@ require("catppuccin").setup({
   no_underline = false, -- Force no underline
   styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
     comments = { "italic" }, -- Change the style of comments
-    conditionals = { "italic" },
-    loops = { "italic" },
-    functions = { "italic" },
+    conditionals = {},
+    loops = {},
+    functions = {},
     keywords = {},
     strings = {},
     variables = {},
