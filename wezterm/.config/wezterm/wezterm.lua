5b9f9d9659fba0e23eb20ea6ddadad9299215896
Dhananjay <dhananjay.latkar@gmail.com>
2025-05-09 21:55:19 +0530

    wezterm: enable tab bar

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -32,9 +32,6 @@ config.font_size = 14.0
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
 config.window_decorations = "NONE"
 
--- config.enable_tab_bar = false
-config.hide_tab_bar_if_only_one_tab = true
-
 -- don't ask when closing
 config.window_close_confirmation = "NeverPrompt"
 
@@ -61,5 +58,36 @@ if wezterm.target_triple == "x86_64-pc-windows-msvc" then
   config.window_decorations = "RESIZE"
 end
 
+-- tab bar style
+config.use_fancy_tab_bar = false
+config.tab_max_width = 1600
+
+
+local function tab_title(tab_info)
+  local title = tab_info.tab_title
+  if title and #title > 0 then return title end
+  return tab_info.active_pane.title
+end
+
+wezterm.on("format-tab-title", function(tab, _, _, _, hover, max_width)
+  local background = "#0c0c0c"
+  local foreground = "#b6b8bb"
+
+  if tab.is_active then
+    background = "#25be6a"
+    foreground = "#0c0c0c"
+  elseif hover then
+    background = "#be95ff"
+    foreground = "#0c0c0c"
+  end
+
+  local title = ("  %d %s  "):format(tab.tab_index + 1, tab_title(tab))
+
+  return {
+    { Background = { Color = background } },
+    { Foreground = { Color = foreground } },
+    { Text = title },
+  }
+end)
 
 return config
