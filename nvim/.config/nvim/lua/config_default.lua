bedbf8087dcfcf46387df3dfdd225dadd8366a17
Dhananjay <dhananjay.latkar@gmail.com>
2025-07-20 22:07:23 +0530

    nvim: update lsp for 0.11

--- nvim/.config/nvim/lua/config_default.lua
+++ nvim/.config/nvim/lua/config_default.lua
@@ -25,15 +25,39 @@ M.picker = "mini-pick"
 
 -- LSP
 M.lsp = {
-  -- List of LSP servers to enable
-  -- Use :LspInstallInfo to check available servers
-  -- Use empty table if no settings are required
   servers = {
     lua_ls = {
-      Lua = {
-        workspace = { checkThirdParty = false },
-        telemetry = { enable = false },
-        diagnostics = { globals = { "vim" } },
+      enable = true, -- used by custom config
+      on_init = function(client)
+        if client.workspace_folders then
+          local path = client.workspace_folders[1].name
+          if
+            path ~= vim.fn.stdpath("config")
+            and (vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc"))
+          then
+            return
+          end
+        end
+
+        client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+          runtime = {
+            version = "LuaJIT",
+            path = {
+              "lua/?.lua",
+              "lua/?/init.lua",
+            },
+          },
+          -- Make the server aware of Neovim runtime files
+          workspace = {
+            checkThirdParty = false,
+            library = {
+              vim.env.VIMRUNTIME,
+            },
+          },
+        })
+      end,
+      settings = {
+        Lua = {},
       },
     },
   },
--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -3,82 +3,18 @@ return {
   "neovim/nvim-lspconfig",
   lazy = false,
   dependencies = {
-    -- Additional lua configuration, makes nvim stuff amazing
-    { "folke/neodev.nvim" },
-
-    -- Automatically install LSPs to stdpath for neovim
-    { "williamboman/mason-lspconfig.nvim" },
+    { "williamboman/mason.nvim" },
   },
   enabled = vim.g.dha.conf.enable.lsp,
   config = function()
-    local mason = require("mason")
-    local mason_lspconfig = require("mason-lspconfig")
     local servers = vim.g.dha.conf.lsp.servers
-    local server_names = vim.tbl_keys(servers)
-
-    mason.setup()
-
-    -- -- Uninstall servers removed from config
-    -- local registry = require("mason-registry")
-    -- for _, server in pairs(registry.get_installed_package_names()) do
-    -- 	print("Checking server " .. server)
-    -- 	if not vim.tbl_contains(server_names, server) then
-    -- 		print("Removing " .. server)
-    -- 		registry.get_package(server):uninstall()
-    -- 	end
-    -- end
-
-    -- Setup neovim lua configuration
-    require("neodev").setup()
-
-    -- Install new servers added to config
-    mason_lspconfig.setup({
-      ensure_installed = server_names,
-    })
-
-    --  This function gets run when an LSP connects to a particular buffer.
-    local on_attach = function(_, bufnr)
-      vim.api.nvim_set_option_value("omnifunc", "v:lua.vim.lsp.omnifunc", { buf = bufnr })
-    end
-
-    -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-
-    mason_lspconfig.setup_handlers({
-      function(server_name)
-        require("lspconfig")[server_name].setup({
-          capabilities = capabilities,
-          on_attach = on_attach,
-          settings = servers[server_name],
-        })
-      end,
-    })
-
-    local function start_cscope_lsp()
-      local cmd = vim.fn.exepath("cscope_lsp")
-      if cmd == "" then
-        return
-      end
-
-      local root_files =
-        { "cscope.out", "cscope.files", "cscope.in.out", "cscope.out.in", "cscope.out.po", "cscope.po.out" }
-      local paths = vim.fs.find(root_files, { stop = vim.env.HOME })
-      local root_dir = vim.fs.dirname(paths[1])
-
-      if root_dir then
-        vim.lsp.start({
-          name = "cscope_lsp",
-          cmd = { cmd },
-          root_dir = root_dir,
-          filetypes = { "c", "h", "cpp", "hpp" },
-        })
+    local s_names = vim.tbl_keys(servers)
+    for _, s in ipairs(s_names) do
+      local config = servers[s]
+      if config.enable then
+        if not vim.tbl_isempty(config) then vim.lsp.config(s, config) end
+        vim.lsp.enable(s)
       end
     end
-
-    -- vim.api.nvim_create_autocmd("FileType", {
-    --   pattern = { "c", "h", "cpp", "hpp" },
-    --   desc = "Start cscope_lsp",
-    --   callback = start_cscope_lsp,
-    -- })
   end,
 }
