bfdf869b0d1104e32d0b9328933d036d43cef015
Dhananjay <dhananjay.latkar@gmail.com>
2025-07-22 21:42:47 +0530

    nvim: minihues: select from predefined colors

--- nvim/.config/nvim/lua/colors/minihues.lua
+++ nvim/.config/nvim/lua/colors/minihues.lua
@@ -1,10 +1,15 @@
+local colors = {
+  { name = "green", bg = "#1F2C24", fg = "#E7DDC7" },
+  { name = "blue", bg = "#232E3F", fg = "#EEECE3" },
+  { name = "purple", bg = "#020019", fg = "#FEF1EF" },
+}
+
+math.randomseed(vim.uv.hrtime())
+local c = colors[math.random(#colors)]
+
 require("mini.hues").setup({
-  -- green
-  background = "#1F2C24",
-  foreground = "#E7DDC7",
-  -- blue
-  -- background = "#232E3F",
-  -- foreground = "#EEECE3",
+  background = c.bg,
+  foreground = c.fg,
   saturation = "high",
   plugins = {
     default = false,
