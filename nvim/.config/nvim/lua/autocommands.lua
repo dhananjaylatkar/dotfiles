c91465959c10636e3ee94602e78372505b7a12b8
Dhananjay <dhananjay.latkar@gmail.com>
2026-02-03 22:09:42 +0530

    nvim: disable autocmd to remove cursorline from unfocused win

--- nvim/.config/nvim/lua/autocommands.lua
+++ nvim/.config/nvim/lua/autocommands.lua
@@ -77,15 +77,15 @@ autocmd("InsertLeave", {
 autocmd("VimResized", { command = "wincmd =" })
 
 -- show cursorline only in active window
-autocmd({ "WinEnter", "BufEnter" }, {
-  group = vim.api.nvim_create_augroup("active_cursorline", { clear = true }),
-  callback = function()
-    vim.opt_local.cursorline = true
-  end,
-})
-autocmd({ "WinLeave", "BufLeave" }, {
-  group = "active_cursorline",
-  callback = function()
-    vim.opt_local.cursorline = false
-  end,
-})
+-- autocmd({ "WinEnter", "BufEnter" }, {
+--   group = group,
+--   callback = function()
+--     vim.opt_local.cursorline = true
+--   end,
+-- })
+-- autocmd({ "WinLeave", "BufLeave" }, {
+--   group = group,
+--   callback = function()
+--     vim.opt_local.cursorline = false
+--   end,
+-- })
